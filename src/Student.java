f2c136b9595a5b0493a78f0c29e4a4cfb00e57d2
Panha <chhelpanha2@gmail.com>
2024-01-25 20:18:41 +0700

    add private modifier to field

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,25 @@
 public class Student extends Person{
 
-    int numCourse;
-    String course;
-    String grade;
+    private int numCourse;
+    private String course;
+    private String grade;
 
     public Student(String name, String address, int numCourse, String grade, String course) {
-
         super(name, address);
         this.numCourse = numCourse;
         this.grade = grade;
         this.course = course;
     }
 
+    public int getNumCourse() {
+        return numCourse;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
 }
